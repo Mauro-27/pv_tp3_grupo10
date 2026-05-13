ce3b8e25da9cb87b62e7f0abcbf2e95a8318951c
rochaerika775-cell <rochaerika775@gmail.com>
2026-05-13 01:46:59 -0300

    Agrego componente Footer-Erika Rocha

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -1,32 +1,34 @@
-export const proyectoService = (() =>{
-    let proyectos = [
-        {id:1, titulo:"Curso Inicial de HTML", categoria:"Programación", estado: true},
-        {id:2, titulo:"Huerta Escolar Automatizada", categoria:"Robótica", estado: true},
-        {id:3, titulo:"Matemática Interactiva", categoria:"Matemática", estado: true},
-        {id:4, titulo:"Laboratorio Virtual de Ciencias", categoria:"Ciencias", estado: true},
-        {id:5, titulo:"Biblioteca Digital para la Escuela", categoria:"Educación", estado: true}
-    ];
+export const proyectoService = (() => {
+  let proyectos = [
+    { id: 1, titulo: "Curso Inicial de HTML", categoria: "Programación", estado: "Activo" },
+    { id: 2, titulo: "Huerta Escolar Automatizada", categoria: "Robótica", estado: "Activo" },
+    { id: 3, titulo: "Matemática Interactiva", categoria: "Matemática", estado: "Activo" },
+    { id: 4, titulo: "Laboratorio Virtual de Ciencias", categoria: "Ciencias", estado: "Activo" },
+    { id: 5, titulo: "Biblioteca Digital para la Escuela", categoria: "Educación", estado: "Activo" }
+  ];
 
-    const obtenerProyectos = () => {
-        return [...proyectos];
-    };
+  const obtenerProyectos = () => {
+    return [...proyectos];
+  };
 
-       const agregarProyectos = () => {
-        proyectos.push(nuevoProyecto)
-    };
+  const agregarProyecto = (nuevoProyecto) => {
+    proyectos.push(nuevoProyecto);
+  };
 
-       const eliminarProyectos = (id) => {
-        proyectos = proyectos.filter(proyectos => proyectos.id !== id);
-    };
+  const eliminarProyecto = (id) => {
+    proyectos = proyectos.filter((proyecto) => proyecto.id !== id);
+  };
 
-       const buscarProyectos = (texto) => {
-            proyectos.titulo.toLowCase().include(texto.toLowCase())
-    };
+  const buscarProyecto = (texto) => {
+    return proyectos.filter((proyecto) =>
+      proyecto.titulo.toLowerCase().includes(texto.toLowerCase())
+    );
+  };
 
-    return{
-        obtenerProyectos,
-        agregarProyectos,
-        eliminarProyectos,
-        buscarProyectos
-    }
+  return {
+    obtenerProyectos,
+    agregarProyecto,
+    eliminarProyecto,
+    buscarProyecto
+  };
 })();
