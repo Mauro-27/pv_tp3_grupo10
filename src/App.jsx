323545eac99e3ea1ee2f4ce9ba9555aed322fca8
GabriellChau <gabrielhchauque.com>
2026-05-13 20:50:06 -0300

    feat(Header&Nav): añadir componentes al proyecto. Chauque Gabriel Horacio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Footer from "./components/Footer";
 import ListaProyectos from './components/ListaProyectos.jsx'
+import Header from './components/Header';
+import Nav from './components/Nav';
 import './css/App.css';
 
 function App() {
 
   return (
     <div className="app-container">
+      <Header />
+      <Nav />
       <ListaProyectos />
       <Footer />
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Gestión de Proyectos Educativos</h1>
+      <h3>Detalles del proyecto</h3>
+    </header>
+  );
+};
+
+export default Header;
