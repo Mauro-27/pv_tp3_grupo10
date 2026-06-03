284c78180f21da9d1f7f1a3bf54372726c05d4af
rochaerika775-cell <rochaerika775@gmail.com>
2026-06-03 01:24:11 -0300

    Actualizo contenido de Header Nav y FormularioProyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <header>
+            <h1>Sistema de Gestion de Proyectos</h1>
+            <p>Trabajo Practico N° 3 - pARTE 4</p>
+        </header>
+    );
+}
+export default Header;
